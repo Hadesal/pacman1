c676764c7b4264ba99861b55facf17e2cbfc85d6
KrisHub94 <kristianhuber@hotmail.com>
2022-11-21 13:17:04 +0100

    added basic pacman movement

--- pacman/src/index.js
+++ pacman/src/index.js
@@ -1,1 +1,45 @@
-function update() {}
+let velocityX = 1;
+let velocityY = 0;
+let positionX = 0;
+let positionY = 0;
+const PACMAN_EL = document.querySelector("#pacman");
+
+const PACMAN_SOURCE = PACMAN_EL.querySelector("img").src;
+const PACMAN_1 = "../assets/images/pacman/pacman-1.png";
+const PACMAN_2 = "../assets/images/pacman/pacman-2.png";
+const PACMAN_3 = "../assets/images/pacman/pacman-0.png";
+
+function update() {
+    positionX += velocityX;
+    PACMAN_EL.style.left = positionX + "px";
+    positionY += velocityY;
+    PACMAN_EL.style.top = positionY + "px"; 
+    animate(PACMAN_SOURCE,PACMAN_1,PACMAN_2,PACMAN_3);
+}
+
+document.addEventListener("keydown", function() {
+    switch(event.key) {
+        case "ArrowDown": velocityY = 1;
+        velocityX = 0;
+        PACMAN_EL.style.transform = "rotate(90deg)";
+        break;
+        case "ArrowUp": velocityY = -1;
+        velocityX = 0;
+        PACMAN_EL.style.transform = "rotate(-90deg)";
+        break;
+        case "ArrowLeft": velocityX = -1;
+        velocityY = 0;
+        PACMAN_EL.style.transform = "rotate(180deg)";
+        break;
+        case "ArrowRight": velocityX = 1;
+        velocityY = 0;
+        PACMAN_EL.style.transform = "rotate(0deg)";
+        break;
+    }
+})
+function animate(source, pic1, pic2, pic3) {
+    source = pic3;
+    console.log(source);
+}
+
+setInterval(update, 16.67);
